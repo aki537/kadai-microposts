1ddb616e6aa1dd8e0da692fa595cb810f1be972e
aki08 <hideponpon1931@gmail.com>
2020-07-22 03:13:25 +0000

    like, unlike

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,13 +12,16 @@ class User < ApplicationRecord
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
   
+  has_many :favorites
+  has_many :favoriteposts, through: :favorites, source: :micropost
+  
   def follow(other_user)
     unless self == other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
     end
   end
   
-  def unfollow (other_user)
+  def unfollow(other_user)
     relationship = self.relationships.find_by(follow_id: other_user.id)
     relationship.destroy if relationship
   end
@@ -29,6 +32,19 @@ class User < ApplicationRecord
   
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+  
+  def likepost(other_micropost)
+    self.favorites.find_or_create_by(micropost_id: other_micropost.id)
+  end
+  
+  def unlikepost(other_micropost)
+    favorite = self.favorites.find_by(micropost_id: other_micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  def likepost?(other_micropost)
+    self.favoriteposts.include?(other_micropost)
   end
   
 end
